fff4496be889fef61edd6db76b5428239dc24ef9
Payton Swick <payton@foolord.com>
2013-02-06 14:14:01 -0500

    Add card spec

--- lib/cardlike/card.rb
+++ lib/cardlike/card.rb
@@ -5,4 +5,18 @@ class Cardlike::Card
     self.name = options[:name]
     self.text = options[:text]
   end
+
+  def self.card(name, &block)
+    klass = Class.new(Cardlike::Card)
+    constant_name = name.gsub(/\s+/, '_')
+    Object.const_set(constant_name, klass) if not Object.const_defined?(constant_name)
+    c = Object.const_get(constant_name).new
+    c.name = name
+    c.class.class_eval(&block) if block_given?
+  end
+
+  def text(card_text=nil)
+    return @text unless card_text
+    @text = card_text
+  end
 end
---
+++ spec/integration/deck_construction_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe 'Defining a card' do
+  context "using the card DSL" do
+
+    context "for a basic card" do
+      before do
+        #FIXME: I don't want to have to use the full path here.
+        @card = Cardlike::Card.card "Fire Monster" do
+          text "A red-hot monster."
+        end
+      end
+
+      it "creates a Card object" do
+        @card.should be_a_kind_of Cardlike::Card
+      end
+
+      it "properly sets the card name" do
+        @card.name.should eq "Fire Monster"
+      end
+
+      it "properly sets the card text" do
+        @card.text.should eq "A red-hot monster."
+      end
+    end
+
+    context "with a custom field specified inline" do
+      before do
+        include Cardlike::Card
+        @card = card "Four of Spades" do
+#           my :value is 4
+        end
+      end
+
+      it "creates a Card object with the field as a hash key" do
+        @card[:value].should eq 4
+      end
+    end
+
+  end
+end

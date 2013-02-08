c2ab2fb019c1f33618f33021f4828f82d66a051a
Payton Swick <payton@foolord.com>
2013-02-08 13:39:51 -0500

    Add Hand#to_s

--- lib/cardlike/hand.rb
+++ lib/cardlike/hand.rb
@@ -1,3 +1,10 @@
 class Cardlike::Hand < Cardlike::Deck
   # FIXME: add ability to pull out individual cards.
+  
+  def to_s
+    puts "Hand: #{name}"
+    self.each do |card|
+      puts "-> #{card}\n"
+    end
+  end
 end

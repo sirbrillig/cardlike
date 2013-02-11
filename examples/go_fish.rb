7f54a074a4e2c934438326fcc80a8eeb52cc25b4
Payton Swick <payton@foolord.com>
2013-02-11 11:38:45 -0500

    add more to go_fish

--- examples/go_fish.rb
+++ examples/go_fish.rb
@@ -5,14 +5,18 @@ Cardlike.game do
   puts "Setting up..."
 
   type_of_card :playing_card do
+    has :face
     has :value
+    has :suit
   end
 
   deck "Go Fish" do
-    ['Spades', 'Clubs', 'Diamonds', 'Hearts'].each do |suit|
+    ['Spades', 'Clubs', 'Diamonds', 'Hearts'].each do |card_suit|
       ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Jack', 'Queen', 'King', 'Ace'].each_with_index do |name, index|
-        new_playing_card("#{name} of #{suit}") do
+        new_playing_card("#{name} of #{card_suit}") do
           value index
+          face name
+          suit card_suit
         end
       end
     end
@@ -45,6 +49,7 @@ Cardlike.game do
       menu.choices(*current_player.map {|c| c.name })
     end
     target_card = current_player.select { |c| c.name == target_card_name }.first
+    puts "#{target_player.name}, do you have any #{target_card.face}s?\n"
 
     another_turn = false
     if removed = target_player.remove_card_if { |c| c[:value] == target_card[:value] }
@@ -53,32 +58,57 @@ Cardlike.game do
       puts "You get another turn."
       another_turn = true
 
+      # Check for matches.
       matching_sets = current_player.group_by { |c| c[:value] }.select { |k, v| v.size >= 4 }
-      matching_sets.each_key do |value|
-        # we will assume that you can only get a match from a card you just
-        # collected.
-        puts "You have a matching set of #{removed.name}!"
-        current_player.remove_card_if { |c| c[:value] == value }
+      matching_sets.each do |matching_value, cards|
+        matching_card = cards.first
+        puts "You have a matching set of #{matching_card.face}s!"
+        current_player.remove_card_if { |c| c[:value] == matching_value }
         score current_player.name
       end
+
     else
-      puts "Go Fish!"
-      the_deck("Go Fish").draw_into current_player
-      # FIXME: check for matches here too.
+      puts "Go Fish!\n"
+      drawn = the_deck("Go Fish").draw_into current_player
+      puts "You drew a #{drawn.name}."
+
+      # Check for matches.
+      matching_sets = current_player.group_by { |c| c[:value] }.select { |k, v| v.size >= 4 }
+      matching_sets.each do |matching_value, cards|
+        matching_card = cards.first
+        puts "You have a matching set of #{matching_card.face}s!"
+        puts "You get another turn."
+        another_turn = true
+        current_player.remove_card_if { |c| c[:value] == matching_value }
+        score current_player.name
+      end
+
     end
-    # FIXME: check for empty deck.
+
     another_turn
   end
 
   begin
-    puts "Beginning a new turn!"
+    puts "\nBeginning a new turn!"
 
     begin
       another_turn = begin_new_turn 
+
+      if the_deck("Go Fish").empty?
+        puts "\nThe deck is empty!\n"
+        break
+      end
+
     end while another_turn
     players.rotate!
     current_player = players.first
+
+    if the_deck("Go Fish").empty?
+      break
+    end
+
   end until players.any? { |p| p.size < 1 }
+
   puts "The game has ended!"
 
   winner = scores.max_by { |p, s| s }.first
